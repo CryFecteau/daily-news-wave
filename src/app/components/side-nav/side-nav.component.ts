694c041468c819aef03d50aa6533de5bd18918cb
Cry <cry.fecteau@gmail.com>
2023-06-10 17:06:47 -0700

    Styled layouts for sidenav and nav contents using MUI and Tailwind

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
-export class SideNavComponent implements OnInit {
-
-  constructor() { }
+export class SideNavComponent implements AfterViewInit {
+  title = 'NewsProject';
+  @ViewChild(MatSidenav) sideNav!: MatSidenav;
 
   ngOnInit(): void {
-  }
 
+  }
+  constructor(private observer: BreakpointObserver) {
+
+  }
+  ngAfterViewInit(): void {
+    this.sideNav.opened = true;
+    this.observer.observe(['(max-width:800px)'])
+      .subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = "over";
+          this.sideNav.close();
+        } else {
+          this.sideNav.mode = 'side';
+          this.sideNav.open();
+        }
+      })
+  }
 }
