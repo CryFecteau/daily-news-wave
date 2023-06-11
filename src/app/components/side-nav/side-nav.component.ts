22a74b930a68453e42408d7c853e843720204ff4
Cry <cry.fecteau@gmail.com>
2023-06-10 18:37:02 -0700

    Add news service and update styling

--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -1,7 +1,7 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ViewChild } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
-
+import { NewsfeedService } from 'src/app/serivces/newsfeed.service';
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
@@ -9,12 +9,22 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class SideNavComponent implements AfterViewInit {
   title = 'NewsProject';
+  sources: any = [];
+  articles: any = [];
+  selectedSource: string = 'Top 10 Articles';
   @ViewChild(MatSidenav) sideNav!: MatSidenav;
 
   ngOnInit(): void {
-
+    this.newsApi.initArticles().subscribe((res: any) => {
+      console.log(res);
+      this.articles = res.articles;
+    })
+    this.newsApi.initSources().subscribe((res: any) => {
+      console.log(res);
+      this.sources = res.sources;
+    })
   }
-  constructor(private observer: BreakpointObserver) {
+  constructor(private observer: BreakpointObserver, private cd: ChangeDetectorRef, private newsApi: NewsfeedService) {
 
   }
   ngAfterViewInit(): void {
@@ -27,6 +37,21 @@ export class SideNavComponent implements AfterViewInit {
         } else {
           this.sideNav.mode = 'side';
           this.sideNav.open();
+        }
+      })
+    this.cd.detectChanges();
+  }
+  searchSource(source: any) {
+    this.newsApi.getArticlesByID(source.id)
+      .subscribe((res: any) => {
+        this.selectedSource = source.name
+        this.articles = res.articles;
+      })
+    this.observer.observe(['(max-width:800px)'])
+      .subscribe((res) => {
+        if (res?.matches) {
+          this.sideNav.mode = "over";
+          this.sideNav.close();
         }
       })
   }
